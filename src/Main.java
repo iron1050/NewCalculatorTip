752afdf7988298aa1d4b4df67c63cbca3efd1127
BT_3W2_06 <c@>
2023-09-26 12:37:16 -0400

    fixed weird issues

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,18 @@
 
 import java.util.Scanner;
-Public class Main {
-    Scanner scan = new Scanner(System.in);
-    System.out.print("Enter the subtotal of your bill: ");
-    // get the subtotal of the bill, before tip
-    double subTotal = scan.nextDouble();
-    System.out.println();
-    System.out.print("Enter the number of people who you are with, including yourself: ")
-    // get the number of people that there are
-    int numberPeople = scan.nextInt();
-    System.out.println()
-            System.out.println("Enter the percent tip that you want to give: ");
-    // get the percentage of tip that you want to give
-    double tipPercentage = scan.nextDouble;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the subtotal of your bill: ");
+        // get the subtotal of the bill, before tip
+        double subTotal = scan.nextDouble();
+        System.out.println();
+        System.out.print("Enter the number of people who you are with, including yourself: ");
+        // get the number of people that there are
+        int numberPeople = scan.nextInt();
+        System.out.println();
+        System.out.println("Enter the percent tip that you want to give: ");
+        // get the percentage of tip that you want to give
+        double tipPercentage = scan.nextDouble();
+    }
 }
