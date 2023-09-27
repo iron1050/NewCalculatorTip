5a49bf20e9dac6adcecbe18cf3280cb8dc8e5ca6
BT_3W2_06 <c@>
2023-09-27 11:21:02 -0400

    adding more parts

--- src/Main.java
+++ src/Main.java
@@ -11,8 +11,15 @@ public class Main {
         // get the number of people that there are
         int numberPeople = scan.nextInt();
         System.out.println();
-        System.out.println("Enter the percent tip that you want to give: ");
+        System.out.print("Enter the percent tip that you want to give: ");
         // get the percentage of tip that you want to give
         double tipPercentage = scan.nextDouble();
+        tipPercentage/= 100;
+        tipPercentage += 1;
+        double totalWithTip = subTotal * tipPercentage;
+        double perPersonTotal = totalWithTip / numberPeople;
+        totalWithTip = Math.round(totalWithTip * 100.0) / 100.0;
+        perPersonTotal = Math.round(perPersonTotal * 100.0) / 100.0;
+
     }
 }
